7ca956ba1413edc31a900d53103f82ca2caf5526
Christian Perez <perez.christian1123@gmail.com>
2021-12-02 07:01:31 +0800

    Change controller name and use request param

--- src/main/java/com/example/demo/controllers/BusinessController.java
+++ src/main/java/com/example/demo/controllers/BusinessController.java
@@ -4,23 +4,23 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ReviewDto;
 import com.example.demo.services.ReviewService;
 
 @RestController
-@RequestMapping("/reviews")
-public class ReviewController {
+@RequestMapping("/business")
+public class BusinessController {
 	
 	@Autowired
 	ReviewService reviewService;
 	
-	@RequestMapping("/{name}")
-	public List<ReviewDto> getReviewsForBusiness(@PathVariable String name) throws IOException {
-		return reviewService.getReviewsForBusiness(name);
+	@RequestMapping("/reviews")
+	public List<ReviewDto> getReviewsForBusiness(@RequestParam String search) throws IOException {
+		return reviewService.getReviewsForBusiness(search);
 	}
 
 }
